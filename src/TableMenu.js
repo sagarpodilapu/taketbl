3ba65dd58d15d309c6eedc77ae1d5ef4c8b41a6e
Sagar Podilapu <podilapu.sagar@gmail.com>
2019-12-30 20:17:17 +0530

    fixed preview button

--- src/TableMenu.js
+++ src/TableMenu.js
@@ -1,153 +1,246 @@
 import React from "react";
-import reactCSS from 'reactcss';
-import TakeTblParent from './TakeTblParent';
+import reactCSS from "reactcss";
+import TakeTblParent from "./TakeTblParent";
 
 class TableMenu extends TakeTblParent {
-
-   constructor (props,context){
-     super(props);
-     this.handleMultiSelect = this.handleMultiSelect.bind(this);
-      this.headers = {
-        'en':{
-          "Table":"Table",
-          "tableno":"table no",
-          "seats":"seats",
-          "section":"section",
-          "Cancel":"Cancel",
-          "Ok":"Ok",
-          "message":{
-                     "Heading":"Table Error",
-                     "Empty":"cannot be 0 or empty",
-                     "AlreadyExist":" already exists "
-          }
-        },
-        'ru':{
-          "Table":"стол",
-          "tableno":"стол №",
-          "seats":"место",
-          "section":"секция",
-          "Cancel":"Отмена",
-          "Ok":"Хорошо",
-          "message":{
-                     "Heading":"стол ошибка",
-                     "Empty":"не может быть 0 или пустым",
-                     "AlreadyExist":" уже существует "
-          }
-        },
-        'be':{
-          "Table":"стол",
-          "tableno":"стол №",
-          "seats":"место",
-          "section":"секция",
-          "Cancel":"Отмена",
-          "Ok":"Хорошо",
-          "message":{
-            "Heading":"стол ошибка",
-            "Empty":"не может быть 0 или пустым",
-            "AlreadyExist":" уже существует "
-          }
+  constructor(props, context) {
+    super(props);
+    this.handleMultiSelect = this.handleMultiSelect.bind(this);
+    this.headers = {
+      en: {
+        Table: "Table",
+        tableno: "table no",
+        seats: "seats",
+        section: "section",
+        Cancel: "Cancel",
+        Ok: "Ok",
+        message: {
+          Heading: "Table Error",
+          Empty: "cannot be 0 or empty",
+          AlreadyExist: " already exists "
         }
-
-     };
-   }
-   handleMultiSelect = (event) =>{
-     this.setState({menu:"multiselect"});
-   }
-   handleKeyDown = (event) => {
-
-    if (event && event.keyCode){
-     if (event.keyCode===189||event.keyCode===69||event.keyCode===190){
-      event.preventDefault();
-      return false;
-     }
-    }
-    if (event.target && event.target.value && event.target.maxLength && event.target.value.length>event.target.maxLength-1){
-      if (event.keyCode!==8){
+      },
+      ru: {
+        Table: "стол",
+        tableno: "стол №",
+        seats: "место",
+        section: "секция",
+        Cancel: "Отмена",
+        Ok: "Хорошо",
+        message: {
+          Heading: "стол ошибка",
+          Empty: "не может быть 0 или пустым",
+          AlreadyExist: " уже существует "
+        }
+      },
+      be: {
+        Table: "стол",
+        tableno: "стол №",
+        seats: "место",
+        section: "секция",
+        Cancel: "Отмена",
+        Ok: "Хорошо",
+        message: {
+          Heading: "стол ошибка",
+          Empty: "не может быть 0 или пустым",
+          AlreadyExist: " уже существует "
+        }
+      }
+    };
+  }
+  handleMultiSelect = event => {
+    this.setState({ menu: "multiselect" });
+  };
+  handleKeyDown = event => {
+    if (event && event.keyCode) {
+      if (
+        event.keyCode === 189 ||
+        event.keyCode === 69 ||
+        event.keyCode === 190
+      ) {
         event.preventDefault();
         return false;
       }
     }
-   }
-
-
-
-  handleCancel = (e) => {
-
+    if (
+      event.target &&
+      event.target.value &&
+      event.target.maxLength &&
+      event.target.value.length > event.target.maxLength - 1
+    ) {
+      if (event.keyCode !== 8) {
+        event.preventDefault();
+        return false;
+      }
+    }
   };
 
-  handleSaveTable = (event) => {
-    this.setState({"tableinfo":{tableno:1,section:"hello"}});
-    let message= this.headers[this.state.lang].message;
-    this.props.savetable(event,message);
+  handleCancel = e => {};
+
+  handleSaveTable = event => {
+    this.setState({ tableinfo: { tableno: 1, section: "hello" } });
+    let message = this.headers[this.state.lang].message;
+    this.props.savetable(event, message);
     return false;
-  }
+  };
 
-render(){
+  render() {
+    let lang = this.state.lang;
+    let headers = this.headers;
 
-  let lang=this.state.lang;
-  let headers=this.headers;
+    const styles = reactCSS({
+      default: {
+        right: {
+          //float:"right"
+        }
+      }
+    });
 
-  const styles = reactCSS({
-    'default': {
-      right: {
-        //float:"right"
-      },
-    }
-  });
-
-  return(
-
-   <div className="rightmenutbl" ref={this.contextRef} onClick={this.handleClick} view="group">
-    <div className="dropdown">
-     <button view="popup" className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      {headers[lang].Table}
-    </button>
-     <div className="dropdown-menu" aria-labelledby="dropdownMenu2" view="popup" >
-      <form className="px-4 py-3" view="popup" noValidate>
-        <div view="popup" className="form-group form-group-sm">
-            <div view="popup" className="input-group input-group-sm mb-3">
-             <div view="popup" className="input-group-prepend">
-               <span view="popup" className="input-group-text" id="basic-addon1"><i className="far fa-question-circle itable smallfont"><label className="required">*</label></i></span>
-           </div>
-             <input  view="popup" name="tableno" type="number"  required={true} className="form-control" size="6"  onKeyDown={ this.handleKeyDown} placeholder={headers[lang].tableno}   maxLength="3" min="1" max="99" aria-describedby="basic-addon1"/>
-           </div>
-       </div>
-       <div view="popup" className="form-group form-group-sm">
-           <div view="popup" className="input-group input-group-sm mb-3">
-            <div view="popup" className="input-group-prepend">
-              <span view="popup" className="input-group-text" id="basic-addon1"><i className="fas fa-chair itable smallfont" ></i></span>
+    return (
+      <div
+        className="rightmenutbl"
+        ref={this.contextRef}
+        onClick={this.handleClick}
+        view="group"
+      >
+        <div className="dropdown">
+          <button
+            view="popup"
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            {headers[lang].Table}
+          </button>
+          <div
+            className="dropdown-menu"
+            aria-labelledby="dropdownMenu2"
+            view="popup"
+          >
+            <form className="px-4 py-3" view="popup" noValidate>
+              <div view="popup" className="form-group form-group-sm">
+                <div view="popup" className="input-group input-group-sm mb-3">
+                  <div view="popup" className="input-group-prepend">
+                    <span
+                      view="popup"
+                      className="input-group-text"
+                      id="basic-addon1"
+                    >
+                      <i className="far fa-question-circle itable smallfont">
+                        <label className="required">*</label>
+                      </i>
+                    </span>
+                  </div>
+                  <input
+                    view="popup"
+                    name="tableno"
+                    type="number"
+                    required={true}
+                    className="form-control"
+                    size="6"
+                    onKeyDown={this.handleKeyDown}
+                    placeholder={headers[lang].tableno}
+                    maxLength="3"
+                    min="1"
+                    max="99"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+              </div>
+              <div view="popup" className="form-group form-group-sm">
+                <div view="popup" className="input-group input-group-sm mb-3">
+                  <div view="popup" className="input-group-prepend">
+                    <span
+                      view="popup"
+                      className="input-group-text"
+                      id="basic-addon1"
+                    >
+                      <i className="fas fa-chair itable smallfont"></i>
+                    </span>
+                  </div>
+                  <input
+                    view="popup"
+                    name="seats"
+                    required={true}
+                    type="number"
+                    className="form-control"
+                    size="6"
+                    placeholder={headers[lang].seats}
+                    onKeyDown={this.handleKeyDown}
+                    maxLength="2"
+                    min="1"
+                    max="99"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+              </div>
+              <div view="popup" className="form-group form-group-sm">
+                <div view="popup" className="input-group input-group-sm mb-3">
+                  <div view="popup" className="input-group-prepend">
+                    <span
+                      view="popup"
+                      className="input-group-text"
+                      id="basic-addon1"
+                    >
+                      <i className="fas fa-columns itable smallfont"></i>
+                    </span>
+                  </div>
+                  <input
+                    view="popup"
+                    name="name"
+                    type="text"
+                    className="form-control"
+                    size="6"
+                    readOnly={true}
+                    placeholder={headers[lang].section}
+                    maxLength="3"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+              </div>
+              <div
+                className="btn-group mr-3"
+                role="group"
+                aria-label="tablesave"
+              >
+                <button
+                  className="btn btn-cancel mr-1 btn-sm"
+                  type="button"
+                  onClick={this.handleClick}
+                >
+                  {" "}
+                  {headers[lang].Cancel}
+                </button>
+                <button
+                  type="button"
+                  view="popup"
+                  className="btn btn-primary btn-sm"
+                  style={styles.right}
+                  action="savetable"
+                  onClick={this.handleSave}
+                >
+                  {headers[lang].Ok}
+                </button>
+              </div>
+            </form>
+            <div className="d-none dropdown-menu">
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">
+                Merge
+              </a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">
+                Copy
+              </a>
             </div>
-            <input  view="popup" name="seats" required={true} type="number" className="form-control" size="6"   placeholder={headers[lang].seats} onKeyDown={this.handleKeyDown} maxLength="2" min="1" max="99" aria-describedby="basic-addon1" />
           </div>
+        </div>
       </div>
-      <div view="popup" className="form-group form-group-sm">
-          <div view="popup" className="input-group input-group-sm mb-3">
-           <div view="popup" className="input-group-prepend">
-             <span view="popup" className="input-group-text" id="basic-addon1"><i className="fas fa-columns itable smallfont" ></i></span>
-           </div>
-           <input  view="popup" name="name"  type="text" className="form-control" size="6" readOnly={true} placeholder={headers[lang].section}  maxLength="3" aria-describedby="basic-addon1" />
-         </div>
-     </div>
-     <div className="btn-group mr-3" role="group" aria-label="tablesave">
-        <button className="btn btn-cancel mr-1 btn-sm" type="button"  onClick={this.handleClick}>  {headers[lang].Cancel}</button><button type="button" view="popup" className="btn btn-primary btn-sm" style={styles.right}  action="savetable"  onClick={this.handleSave} >{headers[lang].Ok}</button>
-     </div>
-     </form>
-     <div className="d-none dropdown-menu">
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Merge</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Copy</a>
-     </div>
-    </div>
-   </div>
- </div>
-
-   /*
-   <div className="rightmenutbl">
-     <button className="btn btn-cancel mr-1 btn-sm" type="button" action="cancel"  >ClickMe</button>
-   </div>*/
- );
- }
+    );
+  }
 }
 
-export default TableMenu
+export default TableMenu;
--- src/TakeTblParent.js
+++ src/TakeTblParent.js
@@ -1,15 +1,15 @@
 import React from "react";
 class TakeTblParent extends React.Component {
-  constructor(props,context){
+  constructor(props, context) {
     super(props);
-    function setLanguage(){
+    function setLanguage() {
       let lang = navigator.language || navigator.userLanguage;
-      if (!lang){
-         lang='en'
+      if (!lang) {
+        lang = "en";
       }
-      if (lang.trim().length>2){
-        lang = lang.split('-');
-        if (lang.length===2){
+      if (lang.trim().length > 2) {
+        lang = lang.split("-");
+        if (lang.length === 2) {
           lang = lang[0];
         }
       }
@@ -17,116 +17,126 @@ class TakeTblParent extends React.Component {
     }
     this.state.lang = setLanguage();
   }
-  state={
-    lang:'',
-    tables:[],
-    activeTable:{},
-    sections:[],
-    activeSection:{},
-    contextmenu:'',
-    tableinfo:{"tableno":0,"section":"",color:''}
+  state = {
+    lang: "",
+    tables: [],
+    activeTable: {},
+    sections: [],
+    activeSection: {},
+    contextmenu: "",
+    tableinfo: { tableno: 0, section: "", color: "" }
     //buttons:["callback":{"cancel":this.handleCancel}]
+  };
+
+  componentDidMount() {
+    var self = this;
+    let menulist = { section: "#rightmenusection", tbl: ".rightmenutbl" };
+    let actionlist = {
+      cancel: "handleCancel",
+      ok: "handleOk",
+      savetable: "handleSaveTable",
+      savesection: "handleSaveSection"
+    };
+    let fnClick = function(event) {
+      //event for saving the information
+      event.preventDefault();
+      if (
+        event.target.type === "button" &&
+        event.target.getAttribute("action")
+      ) {
+        let action = actionlist[event.target.getAttribute("action")];
+
+        if (self[action]) {
+          self[action](event);
+        }
+      }
+      let view = event.target.getAttribute("view");
+      if (view) {
+        if (self.handleClick) {
+          self.handleClick(event);
+        }
+      } else {
+        document.removeEventListener("click", fnClick);
+      }
+    };
+
+    let fnKeydown = function(event) {
+      if (event.target.type && event.target.type === "number") {
+        if (self.handleKeyDown) {
+          self.handleKeyDown(event);
+        }
+      }
+
+      let view = event.target.getAttribute("view");
+      if (view) {
+        if (self.handleClick) {
+          self.handleClick(event);
+        }
+      } else {
+        document.removeEventListener("click", fnKeydown);
+      }
+      //document.removeEventListener('click',fnKeydown);
+    };
+    document.addEventListener("contextmenu", function(event) {
+      let test = event && event.target;
+      if (!test) return;
+      if (window.mergingItems && window.mergingItems.length > 0) {
+        document.addEventListener("click", fnClick);
+        let rightmenu = document.querySelector(".rightmenutbl");
+
+        if (rightmenu) {
+          rightmenu = rightmenu.cloneNode(true);
+          rightmenu.id = "mergemenu";
+          rightmenu.style.block = "block";
+
+          if (!event.target.parentElement.querySelector("#mergemenu")) {
+            event.target.parentElement.appendChild(rightmenu);
+            rightmenu.style.top = "0px";
+            rightmenu.style.left = "0px";
+          }
+        }
+        return;
+      }
+
+      let obj = event.target.getAttribute("selected")
+        ? event.target
+        : event.target.parentElement &&
+          event.target.parentElement.getAttribute("selected")
+        ? event.target.parentElement
+        : null;
+      if (obj) {
+        event.preventDefault();
+        document.addEventListener("click", fnClick);
+        document.addEventListener("keydown", fnKeydown);
+        let menu = null;
+        let menutype = obj.getAttribute("type");
+        let menus = document.querySelectorAll(menulist[menutype]);
+        menu = menus[0];
+        if (menu) {
+          let menuItem = document.getElementById("m" + obj.id);
+          if (!menuItem) {
+            let nm = menu.cloneNode(true);
+            nm.id = "m" + obj.id;
+            nm.style.top = parseInt(obj.style.top.replace("px", "")) + "px";
+            let left = parseInt(obj.style.left.replace("px", "")) + 35 + "px";
+            nm.style.left = left;
+            nm.style.display = "block";
+            document.getElementById("content").appendChild(nm);
+            menuItem = nm;
+          } else {
+          }
+          window.contextmenuSelected = menuItem;
+        } else {
+          console.log("entering error1");
+        }
+      } else {
+        //console.log(event.target)
+
+        window.contextmenuSelected = null;
+        document.removeEventListener("click", fnClick);
+        document.removeEventListener("keydown", fnKeydown);
+      }
+    });
   }
-
-  componentDidMount(){
-     var self=this;
-     let menulist = {"section":"#rightmenusection","tbl":".rightmenutbl"}
-     let actionlist = {"cancel":"handleCancel","ok":"handleOk","savetable":"handleSaveTable","savesection":"handleSaveSection"};
-     let fnClick= function(event){ //event for saving the information
-       event.preventDefault();
-       if (event.target.type==='button' && event.target.getAttribute("action")){
-         let action=actionlist[event.target.getAttribute("action")];
-
-         if (self[action]){
-              self[action](event)
-         }
-       }
-       let view = event.target.getAttribute('view');
-       if (view){
-         if (self.handleClick){
-           self.handleClick(event);
-         }
-       }else{
-         document.removeEventListener('click',fnClick);
-       }
-     }
-
-
-     let fnKeydown = function(event){
-       if (event.target.type && event.target.type==="number"){
-         if(self.handleKeyDown){
-           self.handleKeyDown(event);
-         }
-       }
-
-       let view = event.target.getAttribute('view');
-       if (view){
-         if (self.handleClick){
-           self.handleClick(event);
-         }
-       }else{
-         document.removeEventListener('click',fnKeydown);
-       }
-       //document.removeEventListener('click',fnKeydown);
-     }
-     document.addEventListener("contextmenu", function(event){
-
-       let test = event && event.target ;
-       if  (!test) return;
-       if (window.mergingItems && window.mergingItems.length>0){
-         document.addEventListener('click', fnClick);
-         let rightmenu=document.querySelector('.rightmenutbl');
-
-         if (rightmenu){
-           rightmenu = rightmenu.cloneNode(true);
-           rightmenu.id="mergemenu";
-           rightmenu.style.block="block";
-
-           if (!event.target.parentElement.querySelector("#mergemenu")){
-             event.target.parentElement.appendChild(rightmenu);
-             rightmenu.style.top = '0px';
-             rightmenu.style.left= '0px';
-           }
-         }
-         return;
-       }
-
-       let obj = event.target.getAttribute("selected")? event.target : (event.target.parentElement &&  event.target.parentElement.getAttribute('selected'))?event.target.parentElement :null ;
-       if(obj){
-         event.preventDefault();
-         document.addEventListener('click', fnClick);
-         document.addEventListener('keydown',fnKeydown);
-         let menu=null;
-         let menutype = obj.getAttribute('type')
-         let menus = document.querySelectorAll(menulist[menutype]);
-         menu = menus[0];
-         if(menu){
-
-           let menuItem = document.getElementById("m"+obj.id)
-           if(!menuItem){
-               let nm=menu.cloneNode(true);
-               nm.id="m"+obj.id
-               nm.style.top=parseInt(obj.style.top.replace('px',''))+'px';
-               let left = parseInt(obj.style.left.replace('px',''))+35+'px';
-               nm.style.left=left;
-               nm.style.display='block';
-               document.getElementById("content").appendChild(nm);
-               menuItem = nm;
-           }else{
-           }
-          window.contextmenuSelected=menuItem;
-
-         }else{
-           console.log("entering error1");
-         }
-       }else{
-         //console.log(event.target)
-
-         window.contextmenuSelected=null;
-         document.removeEventListener("click", fnClick);
-         document.removeEventListener('keydown',fnKeydown);
-       }
-      });
-   }
 }
-export default TakeTblParent
+export default TakeTblParent;
